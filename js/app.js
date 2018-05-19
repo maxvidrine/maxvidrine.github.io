80f0b74172351257d3e58a76a25ae0cf25c9351b
maxvidrine <max.vidrine@gmail.com>
2018-05-18 18:01:33 -0700

    fixed smooth scroll so that it adjusts for navbar height, added dropdown hamburger, added carousel though it has since broken

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ console.log("js running");
 $(document).ready(function(e) {
     console.log("document ready");
 
+    // Navbar fade in gray backdrop on scroll
     $(window).scroll(function () {
         var scrollTop = $(this).scrollTop();
         $('.menu').css({
@@ -14,20 +15,53 @@ $(document).ready(function(e) {
     });
 
     // credit https://css-tricks.com/snippets/jquery/smooth-scrolling/
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        $('html, body').animate({scrollTop: target.offset().top}, 250, function () {
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) {
-                return false;
-            } else {
-                $target.attr('tabindex', '-1');
-                $target.focus();
-            }
+
+    // $('a[href*="#"]').on('click', function (e) {
+    //     e.preventDefault();
+    //     var target = $(this.hash);
+    //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //     $('html, body').animate({scrollTop: target.offset().top}, 250, function () {
+    //         var $target = $(target);
+    //         $target.focus();
+    //         if ($target.is(":focus")) {
+    //             return false;
+    //         } else {
+    //             $target.attr('tabindex', '-1');
+    //             $target.focus();
+    //         }
+    //     });
+    // });
+
+    // Find Height of Navbar
+    
+    
+
+    var navHeight = function () {
+        var menu0 = $('.menu').eq(0).height();
+        var menu1 = $('.menu').eq(1).height();
+        if ($('.menu').eq(0).height() > $('.menu').eq(1).height()) {
+            return $('.menu').eq(0).height()
+        } else {return $('.menu').eq(1).height();}
+    }
+
+    // Smooth Scroll to hashtag anchors
+    $('a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - navHeight()
+        }, 250);
+        return false;
+    });
+
+
+
+    // Carousel Control
+    $(function () {
+
+        $('.carousel-control').click(function (e) {
+            e.preventDefault();
+            $('#myCarousel').carousel($(this).data());
         });
+
     });
 
 
